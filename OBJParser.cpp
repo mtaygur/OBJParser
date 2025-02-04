80f444b5925377ac14baf2cf756f31b9955d93b6
Mehmet Mert Taygur <mtaygur@hotmail.com>
2025-02-05 00:43:47 +0100

    Add project files.

---
+++ OBJParser.cpp
@@ -0,0 +1,12 @@
+﻿// OBJParser.cpp : Defines the entry point for the application.
+//
+
+#include "OBJParser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
