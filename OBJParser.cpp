730cf459034e2fe8b0755880b63356ff4d544f59
Mehmet Mert Taygur <mtaygur@hotmail.com>
2025-02-06 01:47:48 +0100

    Configure remote origin with clean slate

--- OBJParser.cpp
+++ OBJParser.cpp
@@ -1,12 +1,7 @@
-﻿// OBJParser.cpp : Defines the entry point for the application.
-//
+﻿#include "OBJParser.h"
 
-#include "OBJParser.h"
-
-using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
 	return 0;
 }
