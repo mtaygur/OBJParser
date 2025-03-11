9457ae4b8ee5b867fddd41d56f8d2094d0da4db2
Mehmet Mert Taygur <mtaygur@hotmail.com>
2025-03-11 22:18:25 +0100

    Simplify LineSegment class and list generation - Decouple face neighbourhood info

--- OBJParser.h
+++ OBJParser.h
@@ -36,70 +36,40 @@ struct Face
 class LineSegment
 {
 	public:
-	LineSegment(int firstVertexIndex, int secondVertexIndex, const std::set<int>& linkedFaceIndices)
+	LineSegment(int firstVertexIndex, int secondVertexIndex)
 	{
 		if (firstVertexIndex == secondVertexIndex)
 		{
 			throw std::invalid_argument(
 				"First and second vertex indices must be different");
 		}
-		this->data.insert(firstVertexIndex);
-		this->data.insert(secondVertexIndex);
-		this->linkedFaceIndices.insert(linkedFaceIndices.begin(), linkedFaceIndices.end());
+		vertexPair =
+			std::make_pair(std::min(firstVertexIndex, secondVertexIndex), std::max(firstVertexIndex, secondVertexIndex));
 	}
 
-	LineSegment(const std::pair<int, int>& pair, const std::set<int>& linkedFaceIndices)
+	LineSegment(const std::pair<int, int>& pair)
 	{
 		if (pair.first == pair.second)
 		{
 			throw std::invalid_argument(
 				"First and second vertex indices must be different");
 		}
-		this->data.insert(pair.first);
-		this->data.insert(pair.second);
-		this->linkedFaceIndices.insert(linkedFaceIndices.begin(), linkedFaceIndices.end());
+		vertexPair =
+			std::make_pair(std::min(pair.first, pair.second), std::max(pair.first, pair.second));
 	}
 
 	LineSegment()
 	{
 	}
 
-	bool operator==(const LineSegment& other) const
+	std::pair<int, int> getVertexPair() const { return vertexPair; }
+
+	bool operator<(const LineSegment& other) const
 	{
-		return (data == other.data);
+		return vertexPair < other.vertexPair;
 	}
-
-
-	std::pair<int, int> getPair() const
-	{
-		return std::make_pair(firstVertexIndex(), secondVertexIndex());
-	}
-
-	std::set<int> getLinkedFaceIndices() const
-	{
-		return linkedFaceIndices;
-	}
-
-	void addNewFaceIndex(int faceIndex)
-	{
-		linkedFaceIndices.insert(faceIndex);
-	}
-
-	struct Hash
-	{
-		std::size_t operator()(const LineSegment
-							   & lineSegment) const
-		{
-			boost::hash<std::pair<int, int>> hasher;
-			return hasher(lineSegment.getPair());
-		}
-	};
-
 
 	private:
-	int firstVertexIndex() const { return *data.begin(); }
-	int secondVertexIndex() const { return *std::next(data.begin()); }
-	std::set<int> data;
-	std::set<int> linkedFaceIndices{};
+	std::pair<int, int> vertexPair{};
 };
 
