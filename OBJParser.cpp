0f3d4dd1aa19ee2d81539c7f4a326517b78915a3
Mehmet Mert Taygur <mtaygur@hotmail.com>
2025-04-10 02:43:25 +0200

    CppLinter test commit1

--- OBJParser.cpp
+++ OBJParser.cpp
@@ -98,7 +98,7 @@ std::vector<LineSegment> calculateLineSegmentsList(const std::vector<Face>& face
 
 int main(int argc, char* argv[])
 {
-
+	double test_var1;
 	if (argc < 2)
 	{
 		std::cout << "Usage: " << argv[0] << " <filename>" << std::endl;
